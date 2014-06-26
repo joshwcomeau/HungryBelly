6056ca17ae1b9095dcb50afed6f312bf27ed9549
Josh Comeau <joshwcomeau@gmail.com>
2014-06-25 20:24:18 -0400

    Removed CSRF protection (for now)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :null_session
 
-
-
-  private
-  def setup_api
-    @api = Ordrin::APIs.new(ENV['ORDRIN_SECRET'], :test)
-  end
 end
