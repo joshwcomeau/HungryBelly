bb37acec042b870598df214aa9f44e69bc4fda52
ellenlai <miss.ellenlai@gmail.com>
2014-09-28 18:37:22 -0400

    Step 4 - Drop down menu

--- db/schema.rb
+++ db/schema.rb
@@ -29,12 +29,6 @@ ActiveRecord::Schema.define(version: 20140927222254) do
     t.datetime "updated_at"
   end
 
-  create_table "newsletters", force: true do |t|
-    t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "orders", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
