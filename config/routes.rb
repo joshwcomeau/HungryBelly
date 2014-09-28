c4d7c3d337951fc877a1eb5aea2e2cb774d88d96
Josh Comeau <joshwcomeau@gmail.com>
2014-09-28 12:19:46 -0400

    Created an alpha orders route

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,11 @@ Rails.application.routes.draw do
   resources :orders
   resources :menus
   resources :api_tests
-  resources :sampleusers
-
   resources :contacts, only: [:create]
 
 
   post 'place_order' => 'menus#place_order', as: :place_order
-
-  # root 'orders#new'
+  get 'alpha/order' => 'sampleusers#new', as: :alpha_path
 
 
   root 'navigation#coming_soon'
