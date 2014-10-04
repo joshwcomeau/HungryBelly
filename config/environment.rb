4e1b8cc862410c63298d03d1505d182e4c880b5e
Josh Comeau <joshwcomeau@gmail.com>
2014-10-04 17:51:15 -0400

    Integrated sendgrid maybe?

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
