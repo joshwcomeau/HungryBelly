b5b86097dfd52f96522928a580d30e27932962f6
Josh Comeau <joshwcomeau@gmail.com>
2014-10-04 16:38:47 -0400

    Updated SMTP setting sfor heroku

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => 'gmail.com',
+  :user_name            => ENV['EMAIL_ADDR'],
+  :password             => ENV['EMAIL_PASS'],
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  
+}
