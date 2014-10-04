ea6186694183c570a3cb65bd0d4b37c0e1306bec
Josh Comeau <joshwcomeau@gmail.com>
2014-10-04 17:12:41 -0400

    Whoops. really using SendGrid now

--- config/environment.rb
+++ config/environment.rb
@@ -6,11 +6,11 @@ Rails.application.initialize!
 
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => 'gmail.com',
-  :user_name            => ENV['EMAIL_ADDR'],
-  :password             => ENV['EMAIL_PASS'],
-  :authentication       => 'plain',
-  :enable_starttls_auto => true  
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
 }
