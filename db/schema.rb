c8c2fdc3980ba10618a31f436774a1d4728a7f1d
ellenlai <miss.ellenlai@gmail.com>
2014-10-17 08:21:51 -0400

    Forgot to rake

--- db/schema.rb
+++ db/schema.rb
@@ -29,12 +29,6 @@ ActiveRecord::Schema.define(version: 20141011221457) do
     t.datetime "updated_at"
   end
 
-  create_table "newsletters", force: true do |t|
-    t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "orders", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
