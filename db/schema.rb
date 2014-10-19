21efd72054c17a8cea3117e2133a143e5650400a
Josh Comeau <joshwcomeau@gmail.com>
2014-10-19 17:46:46 -0400

    Update to the last commit: Forgot to migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141019201857) do
+ActiveRecord::Schema.define(version: 20141019203013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,11 @@ ActiveRecord::Schema.define(version: 20141019201857) do
     t.integer  "order_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "cuisines_users", id: false, force: true do |t|
+    t.integer "user_id"
+    t.integer "cuisine_id"
   end
 
   create_table "newsletters", force: true do |t|
